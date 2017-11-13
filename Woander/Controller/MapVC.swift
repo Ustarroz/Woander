343fc7005e08e7894951014f1a19c445005c5a83
Ustarroz <robin.ustarroz@epitech.eu>
2017-11-13 19:03:15 +0100

    add a working logout button to the map

--- Woander/Controller/MapVC.swift
+++ Woander/Controller/MapVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import Firebase
 
 class MapVC: UIViewController {
 
@@ -25,11 +26,11 @@ class MapVC: UIViewController {
         marker.title = "Coucou Maman!"
         marker.snippet = "On est la"
         marker.map = mapView
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addMyBtn()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -38,6 +39,32 @@ class MapVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func addMyBtn() {
+        let btn = UIButton(frame: CGRect(x: 20, y: 20, width: 70, height: 50))
+        btn.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        btn.setTitle("Logout", for: .normal)
+        btn.tag = 1
+        btn.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(btn)
+    }
 
+    @objc func buttonAction(sender: UIButton!) {
+        let btnsendtag: UIButton = sender
+        if btnsendtag.tag == 1 {
+            let logoutPopup = UIAlertController(title: "Logout", message: "Are you sure you want to logout?", preferredStyle: .actionSheet)
+            let logoutAction = UIAlertAction(title: "Logout", style: .destructive) { (buttonTaped) in
+                do {
+                    try Auth.auth().signOut()
+                    let authVC = self.storyboard?.instantiateViewController(withIdentifier: "AuthVC") as? AuthVC
+                    self.present(authVC!, animated: true, completion: nil)
+                } catch {
+                    print(error)
+                }
+                
+            }
+            logoutPopup.addAction(logoutAction)
+            present(logoutPopup, animated: true, completion: nil)
+        }
+        
+    }
 }
-
